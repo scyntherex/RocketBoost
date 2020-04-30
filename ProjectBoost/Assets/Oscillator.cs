a9436a5ccebc7095165740947fe4a178124db785
Rone Caballero <rone.caballero@gmail.com>
2020-04-29 18:49:43 -0700

    Oscillator now automatic.

--- ProjectBoost/Assets/Oscillator.cs
+++ ProjectBoost/Assets/Oscillator.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
     [Range(0,1)][SerializeField] float movementFactor;
 
     Vector3 startingPos; 
@@ -19,6 +21,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //set movement factor
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSineWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSineWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
--- ProjectBoost/Assets/Rocket.cs
+++ ProjectBoost/Assets/Rocket.cs
@@ -74,7 +74,7 @@ public class Rocket : MonoBehaviour
 
     private void ApplyThrust()
     {
-        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust *Time.deltaTime);
         if (!audioSource.isPlaying)
         {
             audioSource.PlayOneShot(mainEngine);
